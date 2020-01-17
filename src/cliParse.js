825c5cce48351eb3e731bd29ef7f8de17abd54ca
hoani <hoani.bryson@gmail.com>
2020-01-18 09:08:39 +1300

    CLI parsing completed

--- src/cliParse.js
+++ src/cliParse.js
@@ -1,39 +1,167 @@
+// Copyright © 2020 Hoani Bryson
+// License: MIT (https://mit-license.org/)
+//
+// Cli Parse
+//
+// Parses command line input and returns settings
+//
+
 const arg_parse = require('argparse')
 
+function add_command_generate(subparsers) {
+  const supparser = subparsers.addParser(
+    'generate',
+    {
+      addHelp:true,
+      help: "Generate an empty yaml, json, or toml config file"
+    }
+  );
+  supparser.addArgument(
+    'generate',
+    {
+      help: 'Name for the generated file',
+      defaultValue: null,
+      metavar: 'filename'
+    }
+  );
+}
+
+function args_to_settings_generate(args) {
+  const default_generate_suffix = ".yaml";
+  const default_generate_name = "config";
+
+  if (args.generate === '' || (typeof args.generate !== "string")) {
+    args.generate = default_generate_name;
+  }
+  if (/(.yaml|.json|.toml)$/.exec(args.generate) == null) {
+    args.generate += default_generate_suffix;
+  }
+}
+
+function add_command_verify(subparsers) {
+  const supparser = subparsers.addParser(
+    'verify',
+    {
+      addHelp:true,
+      help: "Verify the correctness of a L3aP configuration file"
+    }
+  );
+  supparser.addArgument(
+    'verify',
+    {
+      help: 'Config filename',
+      defaultValue: null,
+      metavar: 'filename'
+    }
+  );
+}
+
+function args_to_settings_verify(args) {
+  if (args.verify == '' || (typeof args.verify !== 'string')) {
+    args.verify = null;
+  }
+}
+
+function add_command_encode(subparsers) {
+  const supparser = subparsers.addParser(
+    'encode',
+    {
+      addHelp:true,
+      help: "Encode a packet using a L3aP configuration file"
+    }
+  );
+  supparser.addArgument(
+    'encode',
+    {
+      help: 'Config filename',
+      metavar: 'filename'
+    }
+  );
+  supparser.addArgument(
+    'category',
+    {
+      help: 'Packet category, e.g. set, get, pub...',
+    }
+  );
+  supparser.addArgument(
+    'address',
+    {
+      help: 'Path of the data item to be addresses',
+    }
+  );
+  supparser.addArgument(
+    '--payload',
+    {
+      help: 'Value(s) of payload items to be added',
+      nargs: "+"
+    }
+  );
+}
+
+function args_to_settings_encode(args) {
+  if (args.encode == '' || (typeof args.encode !== 'string')) {
+    args.encode = null;
+  }
+}
+
+function add_command_decode(subparsers) {
+  const supparser = subparsers.addParser(
+    'decode',
+    {
+      addHelp:true,
+      help: "Decode a packet using a L3aP configuration file"
+    }
+  );
+  supparser.addArgument(
+    'decode',
+    {
+      help: 'Config filename',
+      metavar: 'filename'
+    }
+  );
+  supparser.addArgument(
+    'packet',
+    {
+      help: 'String to decode',
+    }
+  );
+}
+
+function args_to_settings_decode(args) {
+  if (args.decode == '' || (typeof args.decode !== 'string')) {
+    args.decode = null;
+  }
+}
+
 function configure_parser() {
-  let parser = arg_parse.ArgumentParser();
-  parser.addArgument(
-    '--generate',
-    {
-      help: 'Generate an empty yaml, json, or toml config file',
-      defaultValue: null,
-      metavar: 'filename',
-      dest: 'generate'
-    }
-  );
-  parser.addArgument(
-    '--verify',
-    {
-      help: '',
-      defaultValue: null,
-      metavar: 'filename',
-      dest: 'verify'
-    }
-  );
+  let parser = arg_parse.ArgumentParser({addHelp:true});
+
+  var subparsers = parser.addSubparsers({title:'Commands'});
+
+  add_command_generate(subparsers);
+  add_command_verify(subparsers);
+  add_command_encode(subparsers);
+  add_command_decode(subparsers);
+
   return parser;
 }
 
 function args_to_settings(args) {
-  const default_generate_suffix = ".yaml";
-  const default_generate_name = "config";
 
-  if (args.generate != null) {
-    if (args.generate === '') {
-      args.generate = default_generate_name;
-    }
-    if (/(.yaml|.json|.toml)$/.exec(args.generate) == null) {
-      args.generate += default_generate_suffix;
-    }
+  if ("generate" in args) {
+    args_to_settings_generate(args);
+  }
+
+  if ('verify' in args) {
+    args_to_settings_verify(args);
+  }
+
+  if ('encode' in args) {
+    args_to_settings_encode(args);
+  }
+
+  if ('decode' in args) {
+    args_to_settings_decode(args);
   }
 
   return args;
@@ -55,3 +183,4 @@ if (require.main === module) {
 
   console.log(settings)
 }
+
--- test/cliParse.test.js
+++ test/cliParse.test.js
@@ -4,54 +4,114 @@ const toml = require('toml');
 const yaml = require('js-yaml');
 
 
-describe('Basic CLI commands', function() {
-  it('generate', function() {
-    process.argv = ['node','cli.test.js'];
-    result = cli_parse.cli_parse();
-    assert("generate" in result);
-  });
-  it('validate', function() {
-    process.argv = ['node','cli.test.js'];
-    result = cli_parse.cli_parse();
-    assert("validate" in result);
-  });
-});
-
 describe('Generate CLI', function() {
-  it ('no generate is null by default', function() {
-    process.argv = ['node','cli.test.js'];
-    result = cli_parse.cli_parse();
-    assert.equal(result.generate, null);
-  });
   it ('generate filename json', function() {
-    process.argv = ['node','cli.test.js', '--generate', 'new.json'];
+    process.argv = ['node','cli.test.js', 'generate', 'new.json'];
     result = cli_parse.cli_parse();
     assert.equal(result.generate, 'new.json');
   });
   it ('generate filename yaml', function() {
-    process.argv = ['node','cli.test.js', '--generate', 'new.yaml'];
+    process.argv = ['node','cli.test.js', 'generate', 'new.yaml'];
     result = cli_parse.cli_parse();
     assert.equal(result.generate, 'new.yaml');
   });
   it ('generate filename toml', function() {
-    process.argv = ['node','cli.test.js', '--generate', 'new.toml'];
+    process.argv = ['node','cli.test.js', 'generate', 'new.toml'];
     result = cli_parse.cli_parse();
     assert.equal(result.generate, 'new.toml');
   });
   it ('generate converts to yaml by default', function() {
-    process.argv = ['node','cli.test.js', '--generate', 'new'];
+    process.argv = ['node','cli.test.js', 'generate', 'new'];
     result = cli_parse.cli_parse();
     assert.equal(result.generate, 'new.yaml');
   });
   it ('generate appends to irrelevant extension', function() {
-    process.argv = ['node','cli.test.js', '--generate', 'new.js'];
+    process.argv = ['node','cli.test.js', 'generate', 'new.js'];
     result = cli_parse.cli_parse();
     assert.equal(result.generate, 'new.js.yaml');
   });
   it ('handle empty string', function() {
-    process.argv = ['node','cli.test.js', '--generate', ''];
+    process.argv = ['node','cli.test.js', 'generate', ''];
+    result = cli_parse.cli_parse();
+    assert.equal(result.generate, 'config.yaml');
+  });
+  it ('handle non-string', function() {
+    process.argv = ['node','cli.test.js', 'generate', 2.5];
     result = cli_parse.cli_parse();
     assert.equal(result.generate, 'config.yaml');
   });
 });
 
+describe('Verify CLI', function() {
+  it ('verify filename', function() {
+    process.argv = ['node','cli.test.js', 'verify', 'new.json'];
+    result = cli_parse.cli_parse();
+    assert.equal(result.verify, 'new.json');
+  });
+  it ('empty string goes to null', function() {
+    process.argv = ['node','cli.test.js', 'verify', ''];
+    result = cli_parse.cli_parse();
+    assert.equal(result.verify, null);
+  });
+  it ('non-string goes to null', function() {
+    process.argv = ['node','cli.test.js', 'verify', 0x1234];
+    result = cli_parse.cli_parse();
+    assert.equal(result.verify, null);
+  });
+});
+
+describe('Encode CLI', function() {
+  it ('simple', function() {
+    process.argv = ['node','cli.test.js', 'encode', 'config.json', 'set', 'path/to/item', '--payload', '10'];
+    result = cli_parse.cli_parse();
+    assert.equal(result.encode, 'config.json');
+    assert.equal(result.category, 'set');
+    assert.equal(result.address, 'path/to/item');
+    assert.equal(result.payload.toString(), ['10'].toString());
+  });
+  it ('multi-payload', function() {
+    process.argv = ['node','cli.test.js', 'encode', 'config.json', 'set', 'path/to/item', '--payload', '10', '7aa2', '0000'];
+    result = cli_parse.cli_parse();
+    assert.equal(result.encode, 'config.json');
+    assert.equal(result.category, 'set');
+    assert.equal(result.address, 'path/to/item');
+    assert.equal(result.payload.toString(), ['10', '7aa2', '0000'].toString());
+  });
+  it ('payload optional', function() {
+    process.argv = ['node','cli.test.js', 'encode', 'config.json', 'set', 'path/to/item'];
+    result = cli_parse.cli_parse();
+    assert.equal(result.encode, 'config.json');
+    assert.equal(result.category, 'set');
+    assert.equal(result.address, 'path/to/item');
+    assert.equal(result.payload, null);
+  });
+  it ('empty string goes to null', function() {
+    process.argv = ['node','cli.test.js', 'encode', '', 'set', 'path'];
+    result = cli_parse.cli_parse();
+    assert.equal(result.encode, null);
+  });
+  it ('non-string goes to null', function() {
+    process.argv = ['node','cli.test.js', 'encode', 0x1234, 'set', 'path'];
+    result = cli_parse.cli_parse();
+    assert.equal(result.encode, null);
+  });
+});
+
+describe('Decode CLI', function() {
+  it ('simple', function() {
+    process.argv = ['node','cli.test.js', 'decode', 'config.json', 'A8000:1234:34:34'];
+    result = cli_parse.cli_parse();
+    assert.equal(result.decode, 'config.json');
+    assert.equal(result.packet, 'A8000:1234:34:34');
+  });
+  it ('empty string goes to null', function() {
+    process.argv = ['node','cli.test.js', 'decode', '', 'A8000:1234:34:34'];
+    result = cli_parse.cli_parse();
+    assert.equal(result.decode, null);
+  });
+  it ('non-string goes to null', function() {
+    process.argv = ['node','cli.test.js', 'decode', 0x1234, 'A8000:1234:34:34'];
+    result = cli_parse.cli_parse();
+    assert.equal(result.decode, null);
+  });
+});
